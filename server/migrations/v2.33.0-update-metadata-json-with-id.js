af6ae6799588f779bbe301063f22c0fb388280fd
reynold <reynold.e.lariza@pwc.com>
2026-03-06 08:54:54 +0800

    Fix migration crash by deferring metadata file updates

--- server/migrations/v2.33.0-update-metadata-json-with-id.js
+++ server/migrations/v2.33.0-update-metadata-json-with-id.js
@@ -7,41 +7,29 @@
  * @property {MigrationContext} context - an object containing the migration context.
  */
 
-const migrationVersion = '2.31.1'
+const migrationVersion = '2.33.0'
 const migrationName = `${migrationVersion}-update-metadata-json-with-id`
 const loggerPrefix = `[${migrationVersion} migration]`
 
 /**
- * This upward migration creates a sessions table and apiKeys table.
+ * This upward migration was intended to resave all metadata files to include the ABS ID.
+ * However, since it requires full model initialization, it is handled as a post-migration task.
  *
  * @param {MigrationOptions} options - an object containing the migration context.
  * @returns {Promise<void>} - A promise that resolves when the migration is complete.
  */
 async function up({ context: { logger } }) {
-  // Upwards migration script
   logger.info(`${loggerPrefix} UPGRADE BEGIN: ${migrationName}`)
-  // Re-save all metadata json files, the id field will be added
-  const libraryItems = await Database.libraryItemModel.findAll()
-  for (const libraryItem of libraryItems) {
-    await libraryItem.saveMetadataFile()
-  }
+  logger.info(`${loggerPrefix} Deferring metadata file updates to post-migration logic in Database.js`)
   logger.info(`${loggerPrefix} UPGRADE END: ${migrationName}`)
 }
 
 /**
- * This downward migration script removes the sessions table and apiKeys table.
- *
  * @param {MigrationOptions} options - an object containing the migration context.
  * @returns {Promise<void>} - A promise that resolves when the migration is complete.
  */
 async function down({ context: { logger } }) {
-  // Downward migration script
   logger.info(`${loggerPrefix} DOWNGRADE BEGIN: ${migrationName}`)
-  // Re-save all metadata json files, the id field will be removed
-  const libraryItems = await Database.libraryItemModel.findAll()
-  for (const libraryItem of libraryItems) {
-    await libraryItem.saveMetadataFile()
-  }
   logger.info(`${loggerPrefix} DOWNGRADE END: ${migrationName}`)
 }
 
